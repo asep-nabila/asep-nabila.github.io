152595a84a885d5f65c971b86b9b95c939f64eb9
Asep Maulana Nuriman <maunklana@users.noreply.github.com>
2022-07-02 16:09:53 +0700

    multiple Changes

---
+++ js/app.js
@@ -0,0 +1,130 @@
+$(function() {
+	$.fn.isInViewport = function() {
+		var elementTop = $(this).offset().top;
+		var elementBottom = elementTop + $(this).outerHeight();
+
+		var viewportTop = $(window).scrollTop();
+		var viewportBottom = viewportTop + $(window).height();
+
+		return elementBottom > viewportTop && elementTop < viewportBottom;
+	};
+	
+	$(window).on('resize scroll', function() {
+		if ($('#messagesfromvisitor').isInViewport()) {
+			if($("#messagesfromvisitor>.messagesfromvisitor-container").children().length < 1 && $("#messagesfromvisitor").find(".messagesfromvisitor-error.d-none").length == 1) drawMessages();
+		}
+	});
+	
+	function drawMessagesOnScroll(){
+	   if($("#messagesfromvisitor").scrollTop() > $("#messagesfromvisitor>.messagesfromvisitor-container").height() - $("#messagesfromvisitor").height()-100) {
+			drawMessages();
+	   }
+	}
+	$("#messagesfromvisitor").on('touchmove scroll', function(){drawMessagesOnScroll();});
+	
+	let playercontrolertimeout;
+	$("#player-elem").on("click touchend", function(){
+		if(typeof playercontrolertimeout !== "undefined") window.clearTimeout(playercontrolertimeout);
+		
+		$playsbutton = $(this);
+		$playsbutton.css('opacity', '1');
+		$("#player-elem>div>div>small").removeClass('d-none');
+		playercontrolertimeout = setTimeout(function(){
+			$playsbutton.css('opacity', '0.5');
+			$playsbutton.blur();
+			$("#player-elem>div>div>small").addClass('d-none');
+			playercontrolertimeout = undefined;
+		}, 5000);
+	});
+	
+	$("#messagesfromvisitor-submit-form").on("submit", function(e){
+		e.preventDefault();
+		submitedform = $(this);
+		$(this).validate({
+			highlight: function(element, errorClass, validClass) {
+				$(element).addClass("is-invalid");
+			},
+			unhighlight: function(element, errorClass, validClass) {
+				$(element).removeClass("is-invalid");
+			},
+			errorClass: "invalid-feedback visible",
+			errorElement: "div",
+			rules: {
+				name: "required",
+				message: {
+					required: true,
+					minlength: 10
+				}
+			},
+			messages: {
+				name: '<i class="bi bi-exclamation-circle"></i> Silahkan isikan nama',
+				message: {
+					required: '<i class="bi bi-exclamation-circle"></i> Silahkan isikan ucapan & doa',
+					minlength: '<i class="bi bi-exclamation-circle"></i> Isikan minimal 10 karakter'
+				}
+			}
+		});
+		if($(this).valid()) {
+			if(typeof grecaptcha !== "undefined"){
+				submitVisitorMessagesError = [];
+			
+				submitmessageform = $(this);
+				submitmessagebtn = submitmessageform.find("button[type=submit]");
+				submitmessagebtn.html('<span class="d-none spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> loading...');
+				submitmessagebtn.prop('disabled', true);
+			
+				grecaptcha.ready(() => {
+					grecaptcha.execute(config.grecaptchasitekey, {action: 'submit'}).then(function(token) {
+						console.log(token);
+						
+						getInsertMessageParams = new getData({}, {"action":"insertComments", "grespon":token});
+						
+						var settings = {
+							"url": `${config.appscript.baseurl}${config.appscript.deploymentid}/exec?${getInsertMessageParams.params()}`,
+							"method": "POST",
+							"timeout": 0,
+							"headers": {
+								"Content-Type": "application/x-www-form-urlencoded",
+							},
+							"redirect": "follow",
+							"data": submitedform.serialize()
+						};
+
+						$.ajax(settings).done((response) => {
+							if(response.statusCode == 1){
+								submitmessageform.trigger("reset");
+								submitmessageform.find("input[name=name]").val(capitalizing(kepada));
+								saveMessages(response.data);
+								drawMessages({loadnew : false});
+							}else{
+								submitVisitorMessagesError.push(response.statusText);
+							}
+						}).fail((jqXHR, textStatus) => {
+							loadNewMessagesXhrErrors.push(`${textStatus}: Tidak dapat terhubung, pastikan perangkat terhubung dan Internet stabil.`);
+						}).always(() => {
+							Swal.fire({
+								icon: 'error',
+								text: submitVisitorMessagesError.join(),
+								confirmButtonColor: '#991188', //Warna kesukaan Nabila
+							});
+							
+							submitmessagebtn.html('Kirim');
+							submitmessagebtn.prop('disabled', false);
+						});
+					});
+				});
+			}else{
+				Swal.fire({
+					icon: 'error',
+					text: `Error: Tidak dapat terhubung ke Google, pastikan perangkat terhubung dan Internet stabil.`,
+					confirmButtonColor: '#991188', //Warna kesukaan Nabila
+				});
+			}
+		}
+	});
+	
+	$(".messagesfromvisitor-gotnew").find("button").on("click", function(){
+		$(this).parent().addClass("d-none");
+		drawMessages({loadnew : false});
+	});
+});
--- js/functions.js
+++ js/functions.js
@@ -1,120 +1,3 @@
-$(function() {
-	$.fn.isInViewport = function() {
-		var elementTop = $(this).offset().top;
-		var elementBottom = elementTop + $(this).outerHeight();
-
-		var viewportTop = $(window).scrollTop();
-		var viewportBottom = viewportTop + $(window).height();
-
-		return elementBottom > viewportTop && elementTop < viewportBottom;
-	};
-	
-	$(window).on('resize scroll', function() {
-		if ($('#messagesfromvisitor').isInViewport()) {
-			if($("#messagesfromvisitor>.messagesfromvisitor-container").children().length <= 0) drawMessages();
-		}
-	});
-	
-	function drawMessagesOnScroll(){
-	   if($("#messagesfromvisitor").scrollTop() > $("#messagesfromvisitor>.messagesfromvisitor-container").height() - $("#messagesfromvisitor").height()-100) {
-			drawMessages();
-	   }
-	}
-	$("#messagesfromvisitor").on('touchmove scroll', function(){drawMessagesOnScroll();});
-	
-	let playercontrolertimeout;
-	$("#player-elem").on("click touchend", function(){
-		if(typeof playercontrolertimeout !== "undefined") window.clearTimeout(playercontrolertimeout);
-		
-		$playsbutton = $(this);
-		$playsbutton.css('opacity', '1');
-		$("#player-elem>div>div>small").removeClass('d-none');
-		playercontrolertimeout = setTimeout(function(){
-			$playsbutton.css('opacity', '0.5');
-			$playsbutton.blur();
-			$("#player-elem>div>div>small").addClass('d-none');
-			playercontrolertimeout = undefined;
-		}, 5000);
-	});
-	
-	$("#messagesfromvisitor-submit-form").on("submit", function(e){
-		e.preventDefault();
-		submitedform = $(this);
-		$(this).validate({
-			highlight: function(element, errorClass, validClass) {
-				$(element).addClass("is-invalid");
-			},
-			unhighlight: function(element, errorClass, validClass) {
-				$(element).removeClass("is-invalid");
-			},
-			errorClass: "invalid-feedback visible",
-			errorElement: "div",
-			rules: {
-				name: "required",
-				message: {
-					required: true,
-					minlength: 10
-				}
-			},
-			messages: {
-				name: '<i class="bi bi-exclamation-circle"></i> Silahkan isikan nama',
-				message: {
-					required: '<i class="bi bi-exclamation-circle"></i> Silahkan isikan ucapan & doa',
-					minlength: '<i class="bi bi-exclamation-circle"></i> Isikan minimal 10 karakter'
-				}
-			}
-		});
-		if($(this).valid()) {
-			
-			submitmessageform = $(this);
-			submitmessagebtn = submitmessageform.find("button[type=submit]");
-			submitmessagebtn.html('<span class="d-none spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> loading...');
-			submitmessagebtn.prop('disabled', true);
-			
-			grecaptcha.ready(function() {
-				grecaptcha.execute(config.grecaptchasitekey, {action: 'submit'}).then(function(token) {
-					getInsertCommentsParams = new getData({}, {"action":"insertComments", "grespon":token});
-					
-					var settings = {
-						"url": `${config.appscript.baseurl}${config.appscript.deploymentid}/exec?${getInsertCommentsParams.params()}`,
-						"method": "POST",
-						"timeout": 0,
-						"headers": {
-							"Content-Type": "application/x-www-form-urlencoded",
-						},
-						"redirect": "follow",
-						"data": submitedform.serialize()
-					};
-
-					$.ajax(settings).done(function (response) {
-						console.log(response);
-						if(response.statusCode == 1){
-							submitmessageform.trigger("reset");
-							submitmessageform.find("input[name=name]").val(capitalizing(kepada));
-							saveMessages(response.data);
-							drawMessages({loadnew : false});
-						}else{
-							Swal.fire({
-								icon: 'error',
-								text: response.statusText,
-								confirmButtonColor: '#991188', //Warna kesukaan Nabila
-							});
-						}
-						
-						submitmessagebtn.html('submit');
-						submitmessagebtn.prop('disabled', false);
-					});
-				});
-			});
-		}
-	});
-	
-	$(".messagesfromvisitor-gotnew").find("button").on("click", function(){
-		$(this).parent().addClass("d-none");
-		drawMessages({loadnew : false});
-	});
-});
-
 const queryParams = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
 });
@@ -380,11 +263,10 @@ const generateQrBukuTamu = function(){
 	
 	let bukutamudata = {
 		"A": kepada,
-		"D": dari,
-		"X": Math.random()
+		"D": dari
 	};
 	
-	let qrstrbukutamu = btoa(JSON.stringify(bukutamudata));
+	let qrstrbukutamu = JSON.stringify(bukutamudata);
 	
 	$('#qrbukutamu').empty();
 	
@@ -523,74 +405,91 @@ const getNewestMessages = function({gotnewmessages = false} = {}){
 	}
 }
 
-let loadNewMessagesXhr, loadNewMessagesRetry=0;
-
+let loadNewMessagesXhr, loadNewMessagesRetry=0, loadNewMessagesXhrErrors = [];
 const loadNewMessages = function(params = {}, functionCallbak) {
 	limitget = 5;
 	
 	if(typeof loadNewMessagesXhr == "undefined" || typeof params.functionrepeat !== "undefined"){
 		if(typeof params.functionrepeat == "undefined") loadNewMessagesXhr = "on going get message";
 		
-		if(typeof params.sort == "undefined") $messagesLoader.removeClass("d-none");
-		
-		grecaptcha.ready(function() {
-			if(typeof params.functionrepeat == "undefined") loadNewMessagesXhr = "waiting grespon";
-			grecaptcha.execute(config.grecaptchasitekey, {action: 'submit'}).then(function(token) {
-				getCommentsParams = new getData(params, {"action":"getComments","limit":limitget,"grespon":token});
-				commentsUrl = `${config.appscript.baseurl}${config.appscript.deploymentid}/exec?${getCommentsParams.params()}`;
-				$.getJSON( commentsUrl , function( response ) {
-					if(response.statusCode == 1){
-						Object.keys(response.data).forEach(function(key) {
-							saveMessages(response.data[key]);
-						});
-						
-						if(typeof params.sort == "undefined" && response.totalreturn<limitget){
-							$("#messagesfromvisitor").off('touchmove scroll');
-							$("#messagesfromvisitor>.messagesfromvisitor-last").removeClass("d-none");
+		if(typeof grecaptcha !== "undefined"){
+			
+			if(typeof params.sort == "undefined") $messagesLoader.removeClass("d-none");
+			
+			grecaptcha.ready(() => {
+				if(typeof params.functionrepeat == "undefined") loadNewMessagesXhr = "waiting grespon";
+				grecaptcha.execute(config.grecaptchasitekey, {action: 'submit'}).then((token) => {
+					getCommentsParams = new getData(params, {"action":"getComments","limit":limitget,"grespon":token});
+					commentsUrl = `${config.appscript.baseurl}${config.appscript.deploymentid}/exec?${getCommentsParams.params()}`;
+					$.getJSON( commentsUrl ).done((response) => {
+						if(response.statusCode == 1){
+							Object.keys(response.data).forEach(function(key) {
+								saveMessages(response.data[key]);
+							});
+							
+							if(typeof params.sort == "undefined" && response.totalreturn<limitget){
+								$("#messagesfromvisitor").off('touchmove scroll');
+								$("#messagesfromvisitor>.messagesfromvisitor-last").removeClass("d-none");
+							}
+							
+							if(typeof params.functionrepeat !== "undefined"){
+								functionCallbak({gotnewmessages : true});
+							}else{
+								functionCallbak();
+							}
+							
+						}else{
+							if(response.statusText == "Tidak ada pesan"){
+								if(typeof params.sort == "undefined"){
+									$("#messagesfromvisitor").off('touchmove scroll');
+									if($messagesPanel.children().length>0){
+										$("#messagesfromvisitor>.messagesfromvisitor-last").removeClass("d-none");
+									}else{
+										$("#messagesfromvisitor>.messagesfromvisitor-empty").removeClass("d-none");
+									}
+								}
+							}else{
+								loadNewMessagesXhrErrors.push(response.statusText);
+							}
 						}
 						
 						if(typeof params.functionrepeat !== "undefined"){
-							functionCallbak({gotnewmessages : true});
-						}else{
-							functionCallbak();
+							setTimeout(functionCallbak, params.functionrepeat);
 						}
-						
-					}else{
-						if(response.statusText == "Tidak ada pesan"){
-							if(typeof params.sort == "undefined"){
-								$("#messagesfromvisitor").off('touchmove scroll');
-								if($messagesPanel.children().length>0){
-									$("#messagesfromvisitor>.messagesfromvisitor-last").removeClass("d-none");
-								}else{
-									$("#messagesfromvisitor>.messagesfromvisitor-empty").removeClass("d-none");
-								}
-							}
-						}else{
-							if(loadNewMessagesRetry<=3){
-								setTimeout(function() {
-									loadNewMessagesRetry++;
-									loadNewMessages(params);
-								}, 2000);
+						if(typeof getNewestMessagesRunning == "undefined"){
+							if(response.statusText == "Tidak ada pesan" || response.statusCode == 1){
+								getNewestMessages();
 							}else{
-								$("#messagesfromvisitor>.messagesfromvisitor-error").removeClass("d-none");
-								$("#messagesfromvisitor>.messagesfromvisitor-error").append(`<br/><span>${response.statusText}</span>`);
+								$("#messagesfromvisitor").off('touchmove scroll');
 							}
 						}
-					}
-					loadNewMessagesXhr = undefined;
-					$messagesLoader.addClass("d-none");
-					
-					if(typeof params.functionrepeat !== "undefined"){
-						setTimeout(functionCallbak, params.functionrepeat);
-					}
-					if(typeof getNewestMessagesRunning == "undefined"){
-						if(response.statusText == "Tidak ada pesan" || response.statusCode == 1){
-							getNewestMessages();
+					}).fail((jqXHR, textStatus) => {
+						$("#messagesfromvisitor").off('touchmove scroll');
+						
+						if(loadNewMessagesRetry<=3){
+							setTimeout(() => {
+								loadNewMessagesRetry++;
+								loadNewMessages(params);
+							}, 1000);
+						}else{
+							loadNewMessagesXhrErrors.push(`${textStatus}: Tidak dapat terhubung, pastikan perangkat terhubung dan Internet stabil.`);
 						}
-					}
+					}).always(() => {
+						if(loadNewMessagesXhrErrors.length>0){
+							$("#messagesfromvisitor").off('touchmove scroll');
+							$("#messagesfromvisitor>.messagesfromvisitor-error").removeClass("d-none");
+							$("#messagesfromvisitor>.messagesfromvisitor-error").append(`<br/><small>${loadNewMessagesXhrErrors.join()}</small>`);
+						}
+						loadNewMessagesXhr = undefined;
+						$messagesLoader.addClass("d-none");
+					});
 				});
 			});
-		});
+		}else{
+			$("#messagesfromvisitor").off('touchmove scroll');
+			$("#messagesfromvisitor>.messagesfromvisitor-error").removeClass("d-none");
+			$("#messagesfromvisitor>.messagesfromvisitor-error").append(`<br/><small>Error: Tidak dapat terhubung ke Google, pastikan perangkat terhubung dan Internet stabil.</small>`);
+		}
 	}
 }
 
@@ -691,7 +590,6 @@ function isMessagesVisitorGetItemHTML({ timestamp, name, message, colleague, att
 
 
 let visitorMessages = {};
-
 const getSavedMessages = function(){
 	visitorMessages = Object.keys(visitorMessages).sort(function ( a, b ) { return b - a; }).reduce(
 	  (obj, key) => { 
