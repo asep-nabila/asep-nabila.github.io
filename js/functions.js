0680c1c256247f61accf78e57ef8f99bfd12dae6
chickenin <maunklana@users.noreply.github.com>
2022-07-12 01:18:13 +0700

    changes on background

--- js/functions.js
+++ js/functions.js
@@ -242,7 +242,7 @@ const showEnvelope = function(){
 
 const showInvitation = function(){
 	window.history.pushState('', '', window.location.pathname);
-	//$("body").css("background-image", 'url('+$("body").data("background")+')');
+	$("body").css("background-image", 'url('+$("body").data("background")+')');
 	
 	$('.xhidden').each(function() {
 		$(this).addClass('animate__animated animate__slideInUp');
