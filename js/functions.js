a2f863acea1a64cb3e9540b98c666d20e30c7e2c
Asep Maulana Nuriman <maunklana@users.noreply.github.com>
2022-07-11 15:03:09 +0700

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -153,7 +153,7 @@ async function getVisitorIP() {
 
 
 // Initialize the agent at application startup.
-const fpPromise = import('./module/sidikramo.js').then(FingerprintJS => FingerprintJS.load({apiKey: configs.fingerprintjs.publictoken, endpoint: `https://${configs.fingerprintjs.customendpoint}.${configs.pagedomain}`}));
+const fpPromise = import('./module/sidikramo.js').then(FingerprintJS => FingerprintJS.load({apiKey: `${configs.fingerprintjs.publictoken}`, endpoint: `https://${configs.fingerprintjs.customendpoint}.${configs.pagedomain}`}));
 let visitorId = '';
 async function getVisitorId() {	
 	if(typeof localStorage.visitorId != "undefined" && localStorage.visitorId != ''){
