91488066a8123cafd4ad636ab7f06e0d81d626fc
Asep Maulana Nuriman <maunklana@users.noreply.github.com>
2022-07-11 17:05:48 +0700

    yes

--- js/module/main.js
+++
@@ -1,4 +0,0 @@
-const FingerprintJS = require('@fingerprintjs/fingerprintjs-pro');
-// Initialize an agent at application startup.
-const fpPromise = FingerprintJS.load({ apiKey: 'OS3SLXNyklDNGY2qQcMy', endpoint: 'https://sr.asepnabila.link' });
-global.window.fpPromise = fpPromise;
