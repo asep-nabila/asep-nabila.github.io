90475c328cd63f337843b578a75c1e64cddae86d
chickenin <maunklana@users.noreply.github.com>
2022-07-14 21:32:42 +0700

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -2,10 +2,10 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.2/workbox
 // Import and configure the Firebase SDK
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.0/firebase-app-compat.js');
-import { getMessaging, getToken } from 'https://www.gstatic.com/firebasejs/9.9.0/firebase-messaging-sw.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
-const firebaseConfig = {
+firebase.initializeApp({
 	apiKey: "AIzaSyBaaMDEZSfrBule95n_y0RcbnzdVtz6Fp8",
 	authDomain: "wedding-asep-nabila.firebaseapp.com",
 	databaseURL: "https://wedding-asep-nabila-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -14,13 +14,11 @@ const firebaseConfig = {
 	messagingSenderId: "12760851327",
 	appId: "1:12760851327:web:dab336f27bb009bd4bf04a",
 	measurementId: "G-77YBSBL6PY"
-};
+});
 
-const app = initializeApp(firebaseConfig);
+const messaging = firebase.getMessaging();
 
-const messaging = getMessaging(app);
-
-getToken(messaging, { vapidKey: 'BFW5T47GqIr7Sc5zU2L5gG-98hN23hlbgQH3zIQOOhT9rUEe8evsw3L9Dg7K_awzsulDvnujCrWiRoB7aKbE860' }).then((currentToken) => {
+messaging.getToken({ vapidKey: 'BFW5T47GqIr7Sc5zU2L5gG-98hN23hlbgQH3zIQOOhT9rUEe8evsw3L9Dg7K_awzsulDvnujCrWiRoB7aKbE860' }).then((currentToken) => {
 	if (currentToken) {
 		console.log(currentToken);
 		let tokenMessages = {
@@ -35,7 +33,7 @@ getToken(messaging, { vapidKey: 'BFW5T47GqIr7Sc5zU2L5gG-98hN23hlbgQH3zIQOOhT9rUE
 	console.log('An error occurred while retrieving token. ', err);
 });
 
-onMessage(messaging, (payload) => {
+messaging.onMessage((payload) => {
 	console.log('Message received. ', payload);
 	let msgMessages = {
 		eventType: "pushmsg",
