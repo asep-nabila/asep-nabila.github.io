847a7ed988723a8a22e5bcdf79dc32d74cfba194
Asep Maulana Nuriman <maunklana@users.noreply.github.com>
2022-07-15 14:55:50 +0700

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,16 +1,16 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.3/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.2/workbox-sw.js');
 
 const {CacheableResponsePlugin} = workbox.cacheableResponse;
 const {ExpirationPlugin} = workbox.expiration;
 const {registerRoute} = workbox.routing;
-const {StaleWhileRevalidate,CacheFirst} = workbox.strategies;
+const {StaleWhileRevalidate} = workbox.strategies;
 
 // Cache Google Fonts with a stale-while-revalidate strategy, with
 // a maximum number of entries.
 registerRoute(
   ({url}) => url.origin === 'https://fonts.googleapis.com' ||
              url.origin === 'https://fonts.gstatic.com',
-  new CacheFirst({
+  new StaleWhileRevalidate({
     cacheName: 'google-fonts',
     plugins: [
       new ExpirationPlugin({maxEntries: 20}),
@@ -18,6 +18,12 @@ registerRoute(
   }),
 );
 
+registerRoute(
+  ({url}) => url.origin === 'https://cdnjs.cloudflare.com' ||
+             url.origin === 'https://cdn.jsdelivr.net',
+  new CacheFirst();
+);
+  
 registerRoute(
   ({request}) => request.destination === 'script' ||
                  request.destination === 'style' ||
@@ -27,7 +33,7 @@ registerRoute(
 
 registerRoute(
   ({request}) => request.destination === 'image',
-  new CacheFirst({
+  new StaleWhileRevalidate({
     cacheName: 'images',
     plugins: [
       new CacheableResponsePlugin({
