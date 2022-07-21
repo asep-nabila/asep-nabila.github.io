eab794b71053816e98a146e167d107358bc1b7ad
chickenin <maunklana@users.noreply.github.com>
2022-07-22 00:06:53 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -43,6 +43,27 @@ $(function() {
 		
 		showEnvelope();
 	}
+	
+	CalamansiEvents.on('initialized', function (player) {
+		players = player;
+		console.log(players);
+		if (localStorage.backsound) {
+			players.audio.play();
+		}
+	});
+
+	CalamansiEvents.on('trackEnded', function (player) {
+		nextsongs();
+	});
+
+	CalamansiEvents.on('play', function (player) {
+		$("#playindicator").addClass("rotating-spin");
+	});
+
+	CalamansiEvents.on('pause', function (player) {
+		$("a.clmns--control-resume").css("padding", "0.35rem 0.5rem");
+		$("#playindicator").removeClass("rotating-spin");
+	});
 });
 
 if (history.scrollRestoration) {
@@ -52,24 +73,3 @@ if (history.scrollRestoration) {
 		location.href = location.href;
 	};
 }
-
-CalamansiEvents.on('initialized', function (player) {
-	players = player;
-	console.log(players);
-	if (localStorage.backsound) {
-		players.audio.play();
-	}
-});
-
-CalamansiEvents.on('trackEnded', function (player) {
-	nextsongs();
-});
-
-CalamansiEvents.on('play', function (player) {
-	$("#playindicator").addClass("rotating-spin");
-});
-
-CalamansiEvents.on('pause', function (player) {
-	$("a.clmns--control-resume").css("padding", "0.35rem 0.5rem");
-	$("#playindicator").removeClass("rotating-spin");
-});
