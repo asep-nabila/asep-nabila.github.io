e0cdcb15ba0993bc06e4dec69f86a7751673364f
chickenin <maunklana@users.noreply.github.com>
2022-07-22 00:02:16 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -52,3 +52,24 @@ if (history.scrollRestoration) {
 		location.href = location.href;
 	};
 }
+
+CalamansiEvents.on('initialized', function (player) {
+	players = player;
+	console.log(players);
+	if (localStorage.backsound) {
+		players.audio.play();
+	}
+});
+
+CalamansiEvents.on('trackEnded', function (player) {
+	nextsongs();
+});
+
+CalamansiEvents.on('play', function (player) {
+	$("#playindicator").addClass("rotating-spin");
+});
+
+CalamansiEvents.on('pause', function (player) {
+	$("a.clmns--control-resume").css("padding", "0.35rem 0.5rem");
+	$("#playindicator").removeClass("rotating-spin");
+});
