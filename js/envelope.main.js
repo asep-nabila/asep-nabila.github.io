59db0e228ed6a263dcb37630293c8384b02f4380
chickenin <maunklana@users.noreply.github.com>
2022-07-21 23:40:14 +0700

    Update envelope.main.js

--- js/envelope.main.js
+++ js/envelope.main.js
@@ -362,29 +362,29 @@ clipboardScript.onload = function(){
 		let clipboardVar = $(this).data("clipboard-var").split("-");
 		$(this).attr("data-clipboard-text", config[clipboardVar[0]][clipboardVar[1]]);
 	});
+	
+	Calamansi.autoload();
+						
+	CalamansiEvents.on('initialized', function (player) {
+		players = player;
+		console.log(players);
+		if (localStorage.backsound) {
+			players.audio.play();
+		}
+	});
+
+	CalamansiEvents.on('trackEnded', function (player) {
+		nextsongs();
+	});
+
+	CalamansiEvents.on('play', function (player) {
+		$("#playindicator").addClass("rotating-spin");
+	});
+
+	CalamansiEvents.on('pause', function (player) {
+		$("a.clmns--control-resume").css("padding", "0.35rem 0.5rem");
+		$("#playindicator").removeClass("rotating-spin");
+	});
 }
 
 generateQrBukuTamu();
-
-Calamansi.autoload();
-						
-CalamansiEvents.on('initialized', function (player) {
-	players = player;
-	console.log(players);
-	if (localStorage.backsound) {
-		players.audio.play();
-	}
-});
-
-CalamansiEvents.on('trackEnded', function (player) {
-	nextsongs();
-});
-
-CalamansiEvents.on('play', function (player) {
-	$("#playindicator").addClass("rotating-spin");
-});
-
-CalamansiEvents.on('pause', function (player) {
-	$("a.clmns--control-resume").css("padding", "0.35rem 0.5rem");
-	$("#playindicator").removeClass("rotating-spin");
-});
