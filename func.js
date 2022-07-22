7a409ef0f3ec32bff2c1cbb9474ca3ff3f141bed
Asep Maulana Nuriman <maunklana@users.noreply.github.com>
2022-07-22 18:55:16 +0700

    Update func.js

--- func.js
+++ func.js
@@ -111,6 +111,6 @@ const showEnvelope = function(){
 			swallAskFrom();
 		}
 		
-		$('head').append('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" rel="preload" as="style" crossorigin="anonymous">');
+		$('head').append('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">');
 	})
 }
