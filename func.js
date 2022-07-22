8caeba9ce0cca0cfbf76a0276f6c9f4d448e619d
chickenin <maunklana@users.noreply.github.com>
2022-07-22 23:39:25 +0700

    Update func.js

--- func.js
+++ func.js
@@ -96,10 +96,10 @@ const showEnvelope = function(){
 		imageAlt: 'Asep Maulana Nuriman (Maunklana) & Nabila Dea Santika (Dila)',
 		didRender: () => {
 			document.head.innerHTML += '<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css" rel="stylesheet" as="font">';
-			document.head.innerHTML += '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">';
 			document.head.innerHTML += '<link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" rel="stylesheet"/>';
 		}
 	}).then((result) => {
+		document.head.innerHTML += '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">';
 		showInvitation();
 		if(typeof kepada == 'undefined' || kepada == ''){
 			swallAskName(swallAskFrom);
