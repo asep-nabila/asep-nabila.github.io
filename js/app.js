276b631636024d84844589ff01b070d343d0a6fc
chickenin <maunklana@users.noreply.github.com>
2022-07-24 23:29:05 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 //app.js
 $(function() {
-	$("#main-css").prop("disabled", false);
 	$('head').append('<link href="https://fonts.googleapis.com/css2?family=Playball&display=swap" rel="stylesheet">');
 	$('head').append('<link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet">');
 	
