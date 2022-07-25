9231627695f21c2ee2599f651c3e5e8f47a4ad97
Asep Maulana Nuriman <maunklana@users.noreply.github.com>
2022-07-25 15:05:00 +0700

    Update envelope.js

--- js/envelope.js
+++ js/envelope.js
@@ -403,10 +403,9 @@ window.history.pushState('', '', window.location.pathname);
 
 getVisitorIP();
 
-appendscript('https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.min.js', 'defer')
-.then(
-	elem.onload = () => {console.log('jquery.validate.min.js Loaded')}
-);
+appendscript('https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.min.js', 'defer').onload = () => {
+	console.log('jquery.validate.min.js Loaded');
+}
 
 let recaptchaScript = document.createElement('script');
 recaptchaScript.src = 'https://www.google.com/recaptcha/api.js?render=6LfhB5wgAAAAAE2vZtWH91E7daPM-KMjdem0uptU';
