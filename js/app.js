9558bd4921be8a571422ab3563b201ef70c44e79
chickenin <maunklana@users.noreply.github.com>
2022-07-29 22:18:29 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -152,32 +152,32 @@ playersPlayTimeout;
 const showInvitation = function(){
 	preloadSound('music/'+playlist[cpi].file).oncanplaythrough = function(){
 		console.log("music loaded");
-	}
-	createcalamnsielement();
-	appendscript('https://cdn.jsdelivr.net/gh/asep-nabila/calamansi-js@master/dist/calamansi.min.js', 'async').onload = function(){		
-		CalamansiEvents.on('initialized', function (player) {
-			players = player;
-			if (localStorage.backsound == "true") {
-				playersPlayTimeout = setTimeout(function(){
-					players.audio.play();
-				}, 1000);
-			}
-		});
+		createcalamnsielement();
+		appendscript('https://cdn.jsdelivr.net/gh/asep-nabila/calamansi-js@master/dist/calamansi.min.js', 'async').onload = function(){		
+			CalamansiEvents.on('initialized', function (player) {
+				players = player;
+				if (localStorage.backsound == "true") {
+					playersPlayTimeout = setTimeout(function(){
+						players.audio.play();
+					}, 1000);
+				}
+			});
 
-		CalamansiEvents.on('trackEnded', function (player) {
-			nextsongs();
-		});
+			CalamansiEvents.on('trackEnded', function (player) {
+				nextsongs();
+			});
 
-		CalamansiEvents.on('play', function (player) {
-			$("#playindicator").addClass("rotating-spin");
-		});
+			CalamansiEvents.on('play', function (player) {
+				$("#playindicator").addClass("rotating-spin");
+			});
 
-		CalamansiEvents.on('pause', function (player) {
-			$("a.clmns--control-resume").css("padding", "0.35rem 0.5rem");
-			$("#playindicator").removeClass("rotating-spin");
-		});
-		
-		Calamansi.autoload();
+			CalamansiEvents.on('pause', function (player) {
+				$("a.clmns--control-resume").css("padding", "0.35rem 0.5rem");
+				$("#playindicator").removeClass("rotating-spin");
+			});
+			
+			Calamansi.autoload();
+		}
 	}
 	$('#envelope').load('envelope.html', function( response, status, xhr ) {
 		if ( status == "error" ) {
