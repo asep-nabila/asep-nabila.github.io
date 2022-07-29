3fe1cf43b2294e3921418c8ff59a4fbd3480880d
chickenin <maunklana@users.noreply.github.com>
2022-07-29 22:34:35 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -171,33 +171,35 @@ const showInvitation = function(){
 				appendscript('js/envelope.main.js', 'async').onload = function(){
 					showLazyImg();
 					
-					preloadSound('music/'+playlist[cpi].file).oncanplaythrough = function(){
-						console.log("music loaded");
-						createcalamnsielement();
-						appendscript('https://cdn.jsdelivr.net/gh/asep-nabila/calamansi-js@master/dist/calamansi.min.js', 'async').onload = function(){		
-							CalamansiEvents.on('initialized', function (player) {
-								players = player;
-								if (localStorage.backsound == "true") {
-									playersPlayTimeout = setTimeout(function(){
-										players.audio.play();
-									}, 500);
-								}
-							});
+					if(!isCrawler){
+						preloadSound('music/'+playlist[cpi].file).oncanplaythrough = function(){
+							console.log("music loaded");
+							createcalamnsielement();
+							appendscript('https://cdn.jsdelivr.net/gh/asep-nabila/calamansi-js@master/dist/calamansi.min.js', 'async').onload = function(){		
+								CalamansiEvents.on('initialized', function (player) {
+									players = player;
+									if (localStorage.backsound == "true") {
+										playersPlayTimeout = setTimeout(function(){
+											players.audio.play();
+										}, 500);
+									}
+								});
 
-							CalamansiEvents.on('trackEnded', function (player) {
-								nextsongs();
-							});
+								CalamansiEvents.on('trackEnded', function (player) {
+									nextsongs();
+								});
 
-							CalamansiEvents.on('play', function (player) {
-								$("#playindicator").addClass("rotating-spin");
-							});
+								CalamansiEvents.on('play', function (player) {
+									$("#playindicator").addClass("rotating-spin");
+								});
 
-							CalamansiEvents.on('pause', function (player) {
-								$("a.clmns--control-resume").css("padding", "0.35rem 0.5rem");
-								$("#playindicator").removeClass("rotating-spin");
-							});
-							
-							Calamansi.autoload();
+								CalamansiEvents.on('pause', function (player) {
+									$("a.clmns--control-resume").css("padding", "0.35rem 0.5rem");
+									$("#playindicator").removeClass("rotating-spin");
+								});
+								
+								Calamansi.autoload();
+							}
 						}
 					}
 				}
