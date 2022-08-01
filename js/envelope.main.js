bdd6f026c9091e7ae3094d535f37750f5ae2cf49
Asep Maulana Nuriman <maunklana@users.noreply.github.com>
2022-08-01 21:13:17 +0700

    Update envelope.main.js

--- js/envelope.main.js
+++ js/envelope.main.js
@@ -61,22 +61,24 @@ let prewedimgs = [{
 ];
 
 $(function() {
-	for (let i = 0; i < prewedimgs.length; ++i) {
-		let img = prewedimgs[i];
-		if($("#gallery-prewed") !== null){
-			$("#gallery-prewed").append(`<div class="grid-item"><a href="${img.href}?${i}" aria-label="${img.alt}"><img class="lazyload grid-img img-fluid" alt="${img.alt}" style="display:unset;" data-src="${img.src}?${i}"></img></a></div>`);
-		}
-		
-		if(i == (prewedimgs.length - 1) && !isCrawler){
-			appendscript('https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js', 'defer').onload = () => {
-				Fancybox.bind("#gallery-prewed > div.grid-item > a", {
-					groupAll : true, // Group all items
-					on : {
-						ready : (fancybox) => {
-							console.log(`fancybox #${fancybox.id} is ready!`);
+	if(!isCrawler){
+		for (let i = 0; i < prewedimgs.length; ++i) {
+			let img = prewedimgs[i];
+			if($("#gallery-prewed") !== null){
+				$("#gallery-prewed").append(`<div class="grid-item"><a href="${img.href}?${i}" aria-label="${img.alt}"><img class="lazyload grid-img img-fluid" alt="${img.alt}" style="display:unset;" data-src="${img.src}?${i}"></img></a></div>`);
+			}
+			
+			if(i == (prewedimgs.length - 1) && !isCrawler){
+				appendscript('https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js', 'defer').onload = () => {
+					Fancybox.bind("#gallery-prewed > div.grid-item > a", {
+						groupAll : true, // Group all items
+						on : {
+							ready : (fancybox) => {
+								console.log(`fancybox #${fancybox.id} is ready!`);
+							}
 						}
-					}
-				});
+					});
+				}
 			}
 		}
 	}
