b9cb2fb5507a5f73f41b413046bf49438d515575
Asep Maulana Nuriman <maunklana@users.noreply.github.com>
2022-08-03 19:04:47 +0700

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -3,7 +3,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.2/workbox
 const {CacheableResponsePlugin} = workbox.cacheableResponse;
 const {ExpirationPlugin} = workbox.expiration;
 const {registerRoute} = workbox.routing;
-const {StaleWhileRevalidate} = workbox.strategies;
+const {StaleWhileRevalidate, CacheFirst} = workbox.strategies;
 
 // Cache Google Fonts with a stale-while-revalidate strategy, with
 // a maximum number of entries.
@@ -17,23 +17,18 @@ registerRoute(
     ],
   }),
 );
-
-registerRoute(
-  ({url}) => url.origin === 'https://cdnjs.cloudflare.com' ||
-             url.origin === 'https://cdn.jsdelivr.net',
-  new StaleWhileRevalidate(),
-);
   
 registerRoute(
   ({request}) => request.destination === 'script' ||
                  request.destination === 'style' ||
+                 request.destination === 'font' ||
 				 request.destination === 'html',
   new StaleWhileRevalidate(),
 );
 
 registerRoute(
   ({request}) => request.destination === 'image',
-  new StaleWhileRevalidate({
+  new CacheFirst({
     cacheName: 'images',
     plugins: [
       new CacheableResponsePlugin({
