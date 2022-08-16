7d0360447c2caa741182c3dfdf61c44a46339507
Asep Maulana Nuriman <maunklana@users.noreply.github.com>
2022-08-16 21:14:46 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -309,7 +309,7 @@ const swallAskFrom = function(){
 				<input type="text" class="form-control swall-input-ask text-light" id="guestdomisili" placeholder="Domisili/Alamat" style="text-transform: uppercase;" required>
 			</div>
 			<select id="geustcollegue" class="form-select swall-input-ask text-light" aria-label="Default select Kenalan" required>
-				<option value="0" selected="">Kenalan/Teman dari?</option>
+				<option value="" selected="">Kenalan/Teman dari?</option>
 				<option value="1">Mempelai pria</option>
 				<option value="2">Mempelai wanita</option>
 				<option value="3">Orang tua mempelai Pria</option>
@@ -331,7 +331,7 @@ const swallAskFrom = function(){
 		}).then((result) => {
 			dari = $("#guestdomisili").val().toUpperCase();
 			kenalan = $("#geustcollegue").val();
-			if(typeof dari !== 'undefined' && dari !== '' && typeof kenalan !== 'undefined' && kenalan !== 0){
+			if(typeof dari !== 'undefined' && dari !== '' && typeof kenalan !== 'undefined' && kenalan !== ''){
 				localStorage.dari = dari;					
 				localStorage.kenalan = kenalan;					
 				swalConfirmBackSound();
